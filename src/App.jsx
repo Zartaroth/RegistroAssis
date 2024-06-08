cb9036843bda1b2910f933070a6c495064a1cb87
Zartaroth <jsalazarbenavente2014@gmail.com>
2024-06-08 08:02:07 -0500

    Arreglar scripts para subir a server cambio client y server carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Formulario from "./pages/Formulario";
+import Tabla from "./components/Tabla";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>Choo Choo! This is an example of a Vite + React app running on Railway.</p>
-      </div>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="table" element={<Tabla />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
